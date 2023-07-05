e66b2375004fcbb4d88543432dc4492798a82fef
Pierre Berkovits <pierre.berkovits@netplus>
2023-07-05 08:58:54 +0200

    keyframes

--- carousel.js
+++ carousel.js
@@ -1,10 +1,10 @@
 const rep = await fetch ("slides.json");
 const slides = await rep.json();
 
-console.log(slides.length);
+/*console.log(slides.length);
 for (let i = 0; i < slides.length; i++) {
     console.log(slides[i].image);
-}
+}*/
 const arol = document.getElementById("arrowleft");
 const aror = document.getElementById("arrowright");
 const bant = document.querySelector(".banner-top");
@@ -13,7 +13,9 @@ const banb = document.querySelector(".banner-bot");
 let imin = 0;
 let imax = slides.length - 1;
 let icur = 0;
-let ipre = 0;
+let ipre = imax;
+
+nextImage();
 
 function nextImageIndex(gd) {
     ipre = icur;
