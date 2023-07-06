4e3e978b120cc41d190d7594b8693a15d3c94679
Pierre Berkovits <pierre.berkovits@netplus>
2023-07-06 17:26:24 +0200

    dots eventListeners

--- carousel.js
+++ carousel.js
@@ -17,16 +17,8 @@ let imax = slides.length - 1;
 let icur = 0;
 let ipre = imax;
 
-for (let j=0; j <= imax; j++) {
-    let dot = document.createElement("i");
-    if (j === ipre) {
-        dot.classList.add("fa-regular", "fa-circle", "dot_selected");
-    } else {
-        dot.classList.add("fa-regular", "fa-circle", "dot");
-    }
-    dots.appendChild(dot);
-}
-
+// Calcul de l'index de l'image suivante : ajout de -1 pour flèche gauche
+//                                         ajout de  1 pour flèche droite
 function nextImageIndex(gd) {
     ipre = icur;
     icur += gd;
@@ -34,8 +26,9 @@ function nextImageIndex(gd) {
     if (icur < imin) {return imax};
     return icur;
 }
-
-function nextImage() {
+// affichage de l'image et de son texte
+// affichage des points
+function showImage() {
     showDot();
     bant.src = slides[icur].image;
     bant.alt = slides[icur].alt;
@@ -44,7 +37,7 @@ function nextImage() {
     banb.alt = slides[ipre].alt;
     tagl.innerHTML = slides[icur].tagLine;
 }
-
+// aficchage des points, celui en cours est sur fond blanc les autres noir
 function showDot() {
     let alldots = document.querySelectorAll(".fa-circle");
     alldots[ipre].classList.remove("dot_selected");
@@ -52,15 +45,41 @@ function showDot() {
     alldots[icur].classList.remove("dot");
     alldots[icur].classList.add("dot_selected");
 }
-
+// ajout des eventsListener sur les points
+function dotEventListener(pdot) {
+    pdot.addEventListener("click", (event) => {
+        let dotcur = event.target;
+        let alldots = document.querySelectorAll(".fa-circle");
+        for (let k = 0; k <= imax; k++) {
+            if (dotcur === alldots[k]) {
+                ipre = icur;
+                icur = k;
+                k = imax + 1
+            }
+        }
+        showImage();
+    })
+}
+// écoute du click sur flèche gauche
 arol.addEventListener("click", () => {
     icur = nextImageIndex(-1);
-    nextImage();
+    showImage();
 });
-
+// écoute du click sur flèche droite
 aror.addEventListener("click", () => {
     icur = nextImageIndex(1);
-    nextImage();
+    showImage();
 });
-
-nextImage();
+// création des points
+for (let j=0; j <= imax; j++) {
+    let dot = document.createElement("i");
+    dotEventListener(dot);
+    if (j === ipre) {
+        dot.classList.add("fa-regular", "fa-circle", "dot_selected");
+    } else {
+        dot.classList.add("fa-regular", "fa-circle", "dot");
+    }
+    dots.appendChild(dot);
+}
+// Appel de la fonction principale d'affichage
+showImage();
